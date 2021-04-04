61a2ad274c5f077df6f81d46f226cf0a3d9d75d8
vagrant <vagrant@vagrant.vm>
2021-04-04 22:38:45 +0000

    Initial commit

---
+++ melons.py
@@ -0,0 +1,66 @@
+# melon_names = {
+#     1: 'Honeydew',
+#     2: 'Crenshaw',
+#     3: 'Crane',
+#     4: 'Casaba',
+#     5: 'Cantaloupe',
+# }
+
+# melon_prices = {
+#     1: 0.99,
+#     2: 2.00,
+#     3: 2.50,
+#     4: 2.50,
+#     5: 0.99,
+# }
+
+# melon_seedlessness = {
+#     1: True,
+#     2: False,
+#     3: False,
+#     4: False,
+#     5: False,
+# }
+
+melons = {
+    'Honeydew' : {
+        'name' : 'Honeydew'
+        'price' : 0.99
+        'seedless' : True
+        'flesh_color' : 
+        'rind_color' :
+        'weight' :
+    },
+    'Crenshaw' : {
+        'name' : 'Crenshaw'
+        'price' : 2.00
+        'seedless' : False
+        'flesh_color' : 
+        'rind_color' :
+        'weight' :
+    },
+    'Crane' : {
+        'name' : 'Crane'
+        'price' : 2.50
+        'seedless' : False
+        'flesh_color' : 
+        'rind_color' :
+        'weight' :
+    },
+    'Casaba' : {
+        'name' : 'Casaba'
+        'price' : 2.50
+        'seedless' : False
+        'flesh_color' : 
+        'rind_color' :
+        'weight' :
+    },
+    'Canteloupe' : {
+        'name' : 'Canteloupe'
+        'price' : 0.99
+        'seedless' : False
+        'flesh_color' : 
+        'rind_color' :
+        'weight' :
+    }
+}
